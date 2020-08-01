e0accdd03e24c54465c4aa16bd0c137f3df1d00b
Milind Pandey (Captain Levi) <47604864+Pranshu2780@users.noreply.github.com>
2020-08-01 20:28:51 +0530

    third

--- app/demo/urls.py
+++ app/demo/urls.py
@@ -3,4 +3,10 @@ from . import views
 
 urlpatterns=[
     path('',views.main,name='main'),
+    path('exams/',views.exams,name='exams'),
+     path('jeemains/',views.jeemains,name='jeemains'),
+     path('jeeadvanced/',views.jeeadvanced,name='jeeadvanced'),
+      path('pdf/',views.pdf,name='jeeadvanced'),
+      path('tenth/',views.tenth,name='tenth'),
+      path('twelveth/',views.twelveth,name='twelveth'),
 ]
--- app/demo/views.py
+++ app/demo/views.py
@@ -5,3 +5,29 @@ from django.shortcuts import render
 def main(request):
     context={}
     return render(request,'demo/main.html',context)
+
+
+def exams(request):
+    context={}
+    return render(request,'demo/exams.html',context)
+  
+  
+def jeemains(request):
+    context={}
+    return render(request,'demo/jeemains.html',context)
+   
+def jeeadvanced(request):
+    context={}
+    return render(request,'demo/jeeadvanced.html',context)
+  
+def pdf(request):
+    context={}
+    return render(request,'demo/pdf.html',context)
+
+def tenth(request):
+    context={}
+    return render(request,'demo/tenth.html',context)
+
+def twelveth(request):
+    context={}
+    return render(request,'demo/twelveth.html',context)
